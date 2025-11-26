c451776a977e52238809070fe40d1c84633e86ef
Shobhit kumar <shobhitkeshri99@gmail.com>
2025-11-27 03:24:14 +0530

    Time: 263 ms (60.23%) | Memory: 152.7 MB (24.94%) - LeetSync

--- 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -2,23 +2,28 @@ class Solution {
 public:
     int n,m;
     int M=1e9+7;
-    int solve(int i,int j,int currSum,vector<vector<int>> &grid,int k,vector<vector<vector<int>>> &dp){
-        if(i==n-1 && j==m-1){
-           return (grid[i][j]+currSum)%k==0;
-        }
-        if(i>=n || j>=m) return 0;
-
-        if(dp[i][j][currSum]!=-1) return dp[i][j][currSum];
-
-        int right=solve(i,j+1,(currSum+grid[i][j])%k,grid,k,dp);
-        int down=solve(i+1,j,(currSum+grid[i][j])%k,grid,k,dp);
-
-        return dp[i][j][currSum]=(right+down)%M;
-    }
     int numberOfPaths(vector<vector<int>>& grid, int k) {
         n=grid.size();
         m=grid[0].size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(m+1,vector<int>(k+1,-1)));
-        return solve(0,0,0,grid,k,dp);
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(m+1,vector<int>(k,0)));
+        
+        for(int rem=0;rem<k;rem++){
+           dp[n-1][m-1][rem]=(grid[n-1][m-1]+rem)%k==0 ?1:0;
+        }
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                for(int rem=0;rem<k;rem++){
+                    if(i==n-1 && j==m-1) continue;
+                     int R=(grid[i][j]+rem)%k;
+                     int right=dp[i][j+1][R];
+                     int down=dp[i+1][j][R];
+
+                     dp[i][j][rem]=(down+right)%M;
+                }
+            }
+        }
+
+        return dp[0][0][0];
     }
 };
