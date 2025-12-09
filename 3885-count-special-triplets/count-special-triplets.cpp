0515a42e823957aa9e5cddbfaa7ac9656b5a4cb9
Shobhit kumar <shobhitkeshri99@gmail.com>
2025-12-09 22:25:58 +0530

    Time: 770 ms (63.14%) | Memory: 402.9 MB (55.47%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -3,21 +3,17 @@ public:
     int M=1e9+7;
     int specialTriplets(vector<int>& nums) {
         int n=nums.size();
-        unordered_map<int,int> m_left,m_right;
-        for(int i=0;i<n;i++){
-            m_right[nums[i]]++;
-        }
-        int count=0;
-        for(int i=0;i<n;i++){
-            m_right[nums[i]]--;
-            if(m_left.find(2*nums[i])!=m_left.end() && m_right.find(2*nums[i])!=m_right.end()){
-                long long first=m_left[2*nums[i]]%M;
-                long long second=m_right[2*nums[i]]%M;
-                long long val=(first*second)%M;
-                count=(count+val)%M;
+        unordered_map<int,int> valid_i,valid_j;
+        
+        int result=0;
+        for(int &num:nums){
+            if(num%2==0){
+                result=(result+valid_j[num/2])%M;
             }
-            m_left[nums[i]]++;
+            valid_j[num]=(valid_j[num]+valid_i[num*2])%M;
+            
+            valid_i[num]++;
         }
-        return count;
+        return result;
     }
 };
