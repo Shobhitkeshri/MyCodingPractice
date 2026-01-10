91eb987050f8f8c818275db339613aa75b11d7ac
Shobhit kumar <shobhitkeshri99@gmail.com>
2026-01-10 15:08:24 +0530

    Time: 11 ms (87.78%) | Memory: 18.1 MB (76.32%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
-    int findCommon(int i,int j,string &s1,string &s2,vector<vector<int>> &dp){
-        if(i>=s1.size() || j>=s2.size()) return 0;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        if(s1[i]==s2[j]){
-            int val=s1[i];
-            return dp[i][j]=val+findCommon(i+1,j+1,s1,s2,dp);
-        }
-        return dp[i][j]=max(findCommon(i,j+1,s1,s2,dp),findCommon(i+1,j,s1,s2,dp));
-    }
     int minimumDeleteSum(string s1, string s2) {
          int firstTotal=0,secondTotal=0;
          int n=s1.size(),m=s2.size();
-         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
          for(int i=0;i<s1.size();i++){
             int val=s1[i];
             firstTotal+=val;
@@ -23,8 +12,19 @@ public:
             int val=s2[i];
             secondTotal+=val;
          }
-        int common=findCommon(0,0,s1,s2,dp);
-        int totalAns=(firstTotal-common)+(secondTotal-common);
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=s1[i-1]+ dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+
+        int totalAns=(firstTotal-dp[n][m])+(secondTotal-dp[n][m]);
         return totalAns;
     }
 };
